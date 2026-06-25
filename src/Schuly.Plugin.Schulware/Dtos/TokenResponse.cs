7f7afe804b40a653276359e76c9ca17262571d79
PianoNic <79938743+PianoNic@users.noreply.github.com>
2026-06-25 08:45:11 +0200

    Removed dead TokenResponse DTO and tidied the Example plugin

--- src/Schuly.Plugin.Schulware/Dtos/TokenResponse.cs
+++
@@ -1,4 +0,0 @@
-namespace Schuly.Plugin.Schulware.Dtos
-{
-    public record TokenResponse([property: System.Text.Json.Serialization.JsonPropertyName("access_token")] string? AccessToken, [property: System.Text.Json.Serialization.JsonPropertyName("refresh_token")] string? RefreshToken);
-}
